4a08ab124814223f294b5d93d054a33a8e4c65af
Daniil Studenikin <daniil.evgenyevich703@gmail.com>
2022-02-09 09:41:03 +0300

    some updates

--- src/main/java/CsvParser.java
+++ src/main/java/CsvParser.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
@@ -17,13 +16,13 @@ public class CsvParser {
 
     public List<String[]> start() {
 
-        BufferedReader reader;
+
         List<String[]> results = new ArrayList<>();
+
         {
-            try {
-                reader = new BufferedReader(new InputStreamReader(
-                        Objects.requireNonNull(getClass().getResourceAsStream("/airports.csv" ))
-                ));
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    Objects.requireNonNull(getClass().getResourceAsStream("/airports.csv"))))) {
+
                 while (reader.readLine() != null) {
                     String[] row = reader.readLine().split(",");
 
@@ -33,7 +32,6 @@ public class CsvParser {
                 }
             } catch (NullPointerException | IOException e) {
                 System.out.println("File not found!");
-                e.printStackTrace();
             }
         }
         return results;
